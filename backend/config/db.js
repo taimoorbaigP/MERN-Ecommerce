e5e773b8d8ff0e525f6c74f300430519bf53e362
taimoorbaigP <taimoorbaig96.tb@gmail.com>
2022-07-21 17:09:38 +0500

    Modeling the Data

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+// database config/connection file
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1) //exit with failure
+  }
+}
+
+export default connectDB
