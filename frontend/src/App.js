fb1982cdc7de6f6e82ced3801a867534de249db6
taimoorbaigP <taimoorbaig96.tb@gmail.com>
2022-08-18 01:50:47 +0500

    Saving token in Postman

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+//import components
 import Header from './components/Header'
 import Footer from './components/Footer'
+// import screens
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import LoginScreen from './screens/LoginScreen'
 import CartScreen from './screens/CartScreen'
 
 const App = () => {
@@ -14,16 +19,15 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route
-              path='/'
-              element={<HomeScreen />}
-              /*component={HomeScreen}*/ exact
-            />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
               <Route path='' element={<CartScreen />} />
             </Route>
+            <Route exact path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,25 +5,45 @@ import {
 } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+
+// Reducers import
 import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+//initial state
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage,
   },
 }
 
